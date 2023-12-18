f2292c2c3d4dce73ff07449d5eda7515adfffcba
Roman <roman@defentry.com>
2023-12-18 20:47:09 +0200

    HW5 completed and updated

--- code/task3.js
+++ code/task3.js
@@ -1,11 +1,11 @@
-document.write(`Функція ggg приймає 2 параметри: анонімну функцію, яка повертає 3 та анонімну функцію, яка повертає 4. 
+document.write(`3. Функція ggg приймає 2 параметри: анонімну функцію, яка повертає 3 та анонімну функцію, яка повертає 4. 
 Поверніть результатом функції ggg суму 3 та 4.<br>`);
 
 
-function ggg(fn1, fn2) {
+function ggg1(fn1, fn2) {
     return fn1() + fn2();
 }
 
-let someResultTask3 = ggg(function () { return 3 }, function () { return 4 });
+let someResultTask3 = ggg1(function () { return 3 }, function () { return 4 });
 
 document.write(`The result = ${someResultTask3}`);
