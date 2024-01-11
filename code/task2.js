9138d24a0ce07bcf7fb82b9e79b5a75cbc6acbeb
Roman <roman@defentry.com>
2024-01-11 19:14:41 +0200

    UPD

--- code/task2.js
+++ code/task2.js
@@ -1,18 +1,20 @@
 document.write(`2. Якщо змінна a більше нуля - то в ggg запишемо функцію, яка виводить один!, інакше запишемо функцію, яка виводить два!<br>`);
 
 const objectTask2 = {
-    userVar : parseInt(prompt("Enter some number", 1,2,3)),
+    userVar: parseInt(prompt("Enter some number", 1, 2, 3)),
 
-    getResult : (number) => {
-            if(number > 0){
-                document.write(`1<br>`);
-            } else if ( number <=0){
-                document.write(`2<br>`);
-            } else {
-                throw new Error(`It's not a number`);
-            }
+    getResult: (number) => {
+        if (number > 0) {
+            return function () { return 1 }
+        } else if (number <= 0) {
+            return function () { return 2 }
+        } else {
+            throw new Error(`It's not a number`);
+        }
 
     }
 }
 
-let ggg = objectTask2.getResult(objectTask2.userVar);
+const ggg = objectTask2.getResult(1);
+
+console.log(ggg());
